554893516665a3e9e19640c83e0c5796626c8a5e
RedBeeツ <77657831+bilal-08@users.noreply.github.com>
2023-11-27 23:24:21 +0530

    Fix():converting image to base64

--- server/util/uploadImage.js
+++ server/util/uploadImage.js
@@ -24,12 +24,10 @@ export const upload = multer({ storage });
 export const uploadImg = async (path) => {
   const file = fs.readFileSync(path)
   const form = new FormData();
-  form.append("image", file)
+  form.append("image", file.toString('base64'))
   const result = await fetch("https://api.imgbb.com/1/upload?key=235ef89fb9a32fa804cddf4a7226d775", {
     method: 'POST',
     body: form,
   }).catch(x => console.error(x))
-const resu = await result.json()
-console.log(resu)
-  return (resu).data.image.url
+  return (await result.json()).data.image.url
 }
