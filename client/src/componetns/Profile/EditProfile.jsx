338dcfa3ea67626087deceed3792e594423d555f
RedBeeツ <77657831+bilal-08@users.noreply.github.com>
2024-01-08 19:26:26 +0530

    Fix(): change the placeholders

--- client/src/componetns/Profile/EditProfile.jsx
+++ client/src/componetns/Profile/EditProfile.jsx
@@ -127,10 +127,10 @@ const [formData,setFormData] = useState({
 
         <form onSubmit={handleform} className="font-inter w-full mt-3 block">              
                 <label for="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">username</label>
-            <input name="username" onChange={handleChange} value={formData.username} type="text" id="username" className="bg-gray-50 mb-3 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Arterus" >
+            <input name="username" onChange={handleChange} value={formData.username} type="text" id="username" className="bg-gray-50 mb-3 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter username" >
             </input>
             <label for="email" className={`block mb-2 text-sm font-medium ${error.iserror && error.msg == "Please Enter a valid email" ? "text-red-700 dark:text-red-500" :  "text-gray-900 dark:text-white"}`}>email</label>
-            <input name="email" onChange={handleChange} value={formData.email} type="text" id="email" className="bg-gray-50 mb-3 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="arterusold@gmail.com" >
+            <input name="email" onChange={handleChange} value={formData.email} type="text" id="email" className="bg-gray-50 mb-3 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter email" >
             
             </input>
             {error.iserror && error.msg == "Please Enter a valid email" && <p className="mt-2 text-sm text-red-600 dark:text-red-500 font-medium">{error.msg}</p>}
